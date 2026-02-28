b81ef77a8abf5140e43ac5036d61c032d2132cbc
Nandini Ramachandran <nandini.ramachandran@gmail.com>
2026-02-28 14:56:42 -0500

    added docket repo

--- src/main/java/com/dailanalytics/dailanalytics/repository/DocketRepo.java
+++ src/main/java/com/dailanalytics/dailanalytics/repository/DocketRepo.java
@@ -1,8 +1,15 @@
 package com.dailanalytics.dailanalytics.repository;
 
+import com.dailanalytics.dailanalytics.models.Docket;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public class DocketRepo {
-    
+public interface DocketRepo extends JpaRepository<Docket, Long> {
+
+    List<Docket> findByCaseEntityId(Long caseId);
+    void deleteByCaseEntityId(Long caseId);
 }
+
