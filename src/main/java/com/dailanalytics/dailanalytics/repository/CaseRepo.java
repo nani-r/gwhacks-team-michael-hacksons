ace1cba137c5bc3af6cf063de42740430b4aca73
Dave <davedeepujose@gmail.com>
2026-02-28 14:46:33 -0500

    Added repo annotations

---
+++ src/main/java/com/dailanalytics/dailanalytics/repository/CaseRepo.java
@@ -0,0 +1,11 @@
+package com.dailanalytics.dailanalytics.repository;
+
+import org.springframework.boot.autoconfigure.data.ConditionalOnRepositoryType;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CaseRepo {
+
+    
+    
+}
---
+++ src/main/java/com/dailanalytics/dailanalytics/repository/SecSource.java
@@ -0,0 +1,8 @@
+package com.dailanalytics.dailanalytics.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SecSource {
+    
+}
