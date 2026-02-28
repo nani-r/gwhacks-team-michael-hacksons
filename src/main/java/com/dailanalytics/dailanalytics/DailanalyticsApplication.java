856271341ec02a02951beb4fb992024461599b9a
Dave <davedeepujose@gmail.com>
2026-02-28 13:27:57 -0500

    adding initial project

---
+++ src/main/java/com/dailanalytics/dailanalytics/DailanalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.dailanalytics.dailanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailanalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailanalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dailanalytics/dailanalytics/DailanalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailanalytics.dailanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailanalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
