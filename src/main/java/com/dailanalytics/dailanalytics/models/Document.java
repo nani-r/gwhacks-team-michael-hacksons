39a88cf8df84347a76a9c0d604501cd2a37ef86c
Junhyok Lee <clamjune1127@gmail.com>
2026-02-28 14:36:03 -0500

    add Document.java

--- src/main/java/com/dailanalytics/dailanalytics/models/Document.java
+++ src/main/java/com/dailanalytics/dailanalytics/models/Document.java
@@ -1,15 +1,32 @@
 package com.dailanalytics.dailanalytics.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Document {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    private Integer caseNumber;
+
+    @Column(length = 64000)
+    private String court;
+
+    private LocalDateTime date;
+
+    @Column(length = 64000)
+    private String link;
+
+    @Column(length = 64000)
+    private String citeOrReference;
 }
